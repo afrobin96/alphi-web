3984ec7a1f47544c19e82ae31f3fb1d9bcbf6023
afrobin96 <afrgmt@gmail.com>
2025-09-26 16:07:46 -0500

    Feat: Add footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Footer {
 
+  alphiServices: String[] = [
+      'Desarrollo e implementación de plataformas educativas',
+      'Desarrollo web profesional',
+      'Capacitación y formación tecnológica',
+      'Soporte técnico y mantenimiento',
+      'Consultoría especializada',
+  ];
+
+  alphiQuickLinks: String[][] = [
+    ['Política de Privacidad', '/policy'],
+    ['Términos de Servicio', '/terms'],
+    ['Preguntas Frecuentes', '/faq'],
+    ['Centro de Ayuda', '/help'],
+  ]
 }

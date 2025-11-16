285a51bce982628e9662e713cc21622e781976ae
afrobin96 <afrgmt@gmail.com>
2025-11-15 19:52:42 -0500

    Feat: add teams form and list

--- src/app/components/admin/teams/teams-form/teams-form.ts
+++ src/app/components/admin/teams/teams-form/teams-form.ts
@@ -1,26 +1,50 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TeamStore } from '../../../../stores/team.store';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-teams-form',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './teams-form.html',
   styleUrl: './teams-form.scss'
 })
-export class TeamsForm {
+export class TeamsForm implements OnInit{
   private fb = inject(FormBuilder);
   private teamStore = inject(TeamStore);
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+
+  editing = false;
+  teamId?: number;
 
   form = this.fb.nonNullable.group({
     name: ['', Validators.required],
     description: ['']
   });
 
-  onSubmit(){
-    if(this.form.valid){
-      this.teamStore.create(this.form.value);
-      this.form.reset();
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')!;
+
+    if(id){
+      this.editing = true;
+      this.teamId = id;
     }
+  }
+
+  onSubmit(){
+    if (this.form.invalid) return;
+
+    const formValue = this.form.value;
+
+    if (this.teamId) {
+      this.teamStore.update(this.teamId, formValue).subscribe();
+      this.router.navigateByUrl('admin/teams');
+    } else {
+      this.teamStore.create(formValue).subscribe();
+      this.router.navigateByUrl('admin/teams');
+    }
+
+    this.form.reset();
   }
 }
--- src/app/components/admin/teams/teams-list/teams-list.ts
+++ src/app/components/admin/teams/teams-list/teams-list.ts
@@ -1,9 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TeamStore } from '../../../../stores/team.store';
+import { TeamData } from '../../../../interfaces/team.interface';
+import { Router, RouterLink } from '@angular/router';
+import { Loader } from "../../../shared/loader/loader";
 
 @Component({
   selector: 'app-teams-list',
-  imports: [],
+  imports: [Loader, RouterLink],
   templateUrl: './teams-list.html',
   styleUrl: './teams-list.scss'
 })
@@ -12,11 +15,18 @@ export class TeamsList implements OnInit{
   private teamStore= inject(TeamStore);
   teams = this.teamStore.teams;
   loading = this.teamStore.loading;
+  router = inject(Router);
+
 
 
   ngOnInit(): void {
     this.teamStore.loadAll();
   }
+
+  edit(team: TeamData){
+      this.router.navigateByUrl(`/admin/teams/edit/${team.id}`);
+  }
+
 
   deleteTeam(id: number){
     if (!confirm('¿Deseas eliminar el equipo?')) return;

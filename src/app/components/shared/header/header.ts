767f4e3ffbc004f63c0e8db6e4cd780218dca248
afrobin96 <afrgmt@gmail.com>
2026-03-27 20:03:22 -0500

    fix link icon to login and consultoria

--- src/app/components/shared/header/header.ts
+++ src/app/components/shared/header/header.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Navbar } from "../navbar/navbar";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, Navbar],
+  imports: [ Navbar],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
-
-  wp_consultoria:string = "https://api.whatsapp.com/send/?phone=573153364927&text=hola quisiera una consultoria";
 }
--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
 
+  wp_consultoria:string = "https://api.whatsapp.com/send/?phone=573153364927&text=hola quisiera una consultoria";
 }

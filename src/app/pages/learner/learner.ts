4269fcba9ced462c93e44d88e7aaf282c48b75bd
afrobin96 <afrgmt@gmail.com>
2026-09-10 22:17:06 -0500

    feat: se termina el diseño de la plataforma entorno admin y learner para comenzar con los demas componentes

--- src/app/pages/learner/learner.ts
+++ src/app/pages/learner/learner.ts
@@ -1,16 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { AuthStore } from '../../stores/auth.store';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 import { Sidebar } from "../../components/shared/sidebar/sidebar";
 import { Topnavbar } from "../../components/shared/topnavbar/topnavbar";
 import { SidebarService } from '../../components/shared/sidebar/sidebar-service';
 
 @Component({
   selector: 'app-learner',
-  imports: [RouterOutlet, MatSidenavModule, MatIconModule, Sidebar, Topnavbar],
+  imports: [RouterOutlet, Sidebar, Topnavbar],
   templateUrl: './learner.html',
   styleUrl: './learner.scss'
 })
